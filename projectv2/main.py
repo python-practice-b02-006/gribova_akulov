a278299398ce21380de493b80f15660af9983962
akulov artem <akulov.aa@phystech.edu>
2020-12-11 03:27:23 +0500

    pep8 + comments

--- projectv2/main.py
+++ projectv2/main.py
@@ -7,25 +7,24 @@ import game2
 import game4
 import experiment
 
+'''
+набор констант 
+'''
 SCREEN_SIZE = (1200, 600)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (255, 250, 205)
 RED = (255, 0, 0)
 YELLOW = (128, 0, 0)
-BROWN = (160, 82, 45) #150, 75, 0
+BROWN = (160, 82, 45)
 
 
 screen = pg.display.set_mode(SCREEN_SIZE)
 pg.init()
 pg.font.init()
 font_1 = pg.font.SysFont("serif", 60)
-
-
-
-
 k = 0
-seq =  30*60*10
+seq = 30 * 60 * 10
 
 pg.mixer.music.load('adventure_music.mp3')
 pg.mixer.music.set_volume(0.1)
@@ -34,12 +33,18 @@ pg.mixer.music.play()
 
 
 class Timer():
+    '''
+    отвечает за время и его отрисовку
+    '''
     global A
+
     def __init__(self, time=seq):
         self.time = time
 
-
     def draw(self):
+        '''
+        рисует полоску времени, как только она заполняется игрок проигрывает и срабатывает функция bad_end
+        '''
         a = (seq - self.time)/seq
         a = int(a * 1200)
         pg.draw.rect(screen, RED, (0, 10, a, 10))
@@ -48,9 +53,15 @@ class Timer():
         screen.blit(text1, (500, 20))
 
     def change(self):
+        '''
+        уменьшает время
+        '''
         self.time -= 1
 
     def bad_end(self):
+        '''
+        в случает проигрыша игрока вызывает черный экран с надписью ты проиграл
+        '''
         if self.time <= 0:
             screen.fill(BLACK)
             f1 = pg.font.Font(None, 36)
@@ -58,6 +69,11 @@ class Timer():
             screen.blit(text1, (500, 300))
 
     def good_end(self):
+        '''
+        считает количество минигр, которые прошел игрок
+        если игрок прошел все, то вызывает зороший конец
+        экран становится черным с надписью ты победил
+        '''
         f1 = pg.font.Font(None, 36)
         text1 = f1.render('You done' + ' ' + str(A) + '/4', 1, RED)
         screen.blit(text1, (1000, 40))
@@ -69,11 +85,17 @@ class Timer():
 
 
 class Map():
+    '''
+    рисует карту
+    '''
     def __init__(self, screen):
         self.screen = screen
         self.coord_points = [[50, 350], [450, 350], [450, 150], [850, 150], [1050, 350]]
 
     def draw(self):
+        '''
+        рисует лабиринт
+        '''
         pg.draw.rect(self.screen, BROWN, (-100, -100, 1500, 800))
 
         pg.draw.rect(self.screen, GRAY, (100, 500, 1100, 100))
@@ -101,30 +123,39 @@ class Map():
         pg.draw.rect(self.screen, GRAY, (1000, 100, 100, 100))
 
 
+'''
+анимация движений героя
+'''
 hero1 = pg.image.load('jump1.png')
 hero2 = pg.image.load('jump2.png')
 hero3 = pg.image.load('jump3.png')
 hero4 = pg.image.load('jump3.png')
+
 
 class Hero():
     def __init__(self, coord=[150, 550]):
         self.coord = coord
 
     def draw(self, screen):
+        '''
+        рисует героя с центром в его координатах
+        '''
         global k
         k += 1
         if (k//10+1) % 5 == 0:
-            screen.blit(hero1,(self.coord[0]-35, self.coord[1]-35))
+            screen.blit(hero1, (self.coord[0]-35, self.coord[1]-35))
         if (k//10+1) % 5 == 1 or (k//10+1) % 5 == 2:            
-            screen.blit(hero2,(self.coord[0]-35, self.coord[1]-35))
+            screen.blit(hero2, (self.coord[0]-35, self.coord[1]-35))
         if (k//10+1) % 5 == 3:            
-            screen.blit(hero3,(self.coord[0]-35, self.coord[1]-35))
+            screen.blit(hero3, (self.coord[0]-35, self.coord[1]-35))
         if (k//10+1) % 5 == 4:      
-            screen.blit(hero4,(self.coord[0]-35, self.coord[1]-35))
-        #pg.draw.circle(screen, BLACK, (self.coord[0], self.coord[1]), 10)
+            screen.blit(hero4, (self.coord[0]-35, self.coord[1]-35))
 
     def night(self):
-        pg.draw.polygon(screen, BLACK, [[0 , 0], [self.coord[0] - 40, self.coord[1] - 40],
+        '''
+        оставляет для игрока видимой щону лишь вокруг героя
+        '''
+        pg.draw.polygon(screen, BLACK, [[0, 0], [self.coord[0] - 40, self.coord[1] - 40],
                                         [self.coord[0] - 40, self.coord[1] + 40], [0, 600]])
         pg.draw.polygon(screen, BLACK, [[0, 600], [self.coord[0] - 40, self.coord[1] + 40],
                                         [self.coord[0] + 40, self.coord[1] + 40], [1200, 600]])
@@ -132,6 +163,7 @@ class Hero():
                                         [self.coord[0] + 40, self.coord[1] - 40], [1200, 0]])
         pg.draw.polygon(screen, BLACK, [[1200, 0], [self.coord[0] + 40, self.coord[1] - 40],
                                         [self.coord[0] - 40, self.coord[1] - 40], [0, 0]])
+
 
 class Manager():
     def __init__(self):
@@ -144,6 +176,9 @@ class Manager():
         self.left_key_pressed = False
 
     def process(self, events, screen):
+        '''
+        процесс игры
+        '''
         done = self.handle_events(events)
         self.map.draw()
         self.hero.draw(screen)
@@ -156,6 +191,9 @@ class Manager():
         return [done, self.where_are_you()]
 
     def handle_events(self, events):
+        '''
+        считывает зажатия и нажатия клавиш
+        '''
         done = False
         for event in events:
             if event.type == pg.QUIT:
@@ -184,11 +222,18 @@ class Manager():
                     self.left_key_pressed = False
     
     def where_are_you(self):
+        '''
+        возвращает координаты героя
+        '''
         for i in range(5):
-            if (self.hero.coord[0] - self.map.coord_points[i][0])**2 + (self.hero.coord[1] - self.map.coord_points[i][1])**2 <250:
+            if (self.hero.coord[0] - self.map.coord_points[i][0]) ** 2 + (self.hero.coord[1] -
+                                                                        self.map.coord_points[i][1]) ** 2 < 250:
                 return i
 
     def teleportation(self):
+        '''
+        в случае если зажата одна из клавиш перемещения меняет положение героя
+        '''
         if self.up_key_pressed:
             if screen.get_at((self.hero.coord[0], self.hero.coord[1] - 37)) == GRAY or \
                     screen.get_at((self.hero.coord[0], self.hero.coord[1] - 37)) == YELLOW:
@@ -207,19 +252,13 @@ class Manager():
                 self.hero.coord[0] += 2
 
 
-
-
 pg.display.set_caption("TRY TO ESCAPE")
 clock = pg.time.Clock()
 
-done = False
-mgr = Manager()
-done1 = (False, False)
-mgr1 = game1.Manager(50, screen)
+A = 0
 
-A=0
 
-dots_figure =[[[180, 588], [255, 513], [330, 588]],
+dots_figure = [[[180, 588], [255, 513], [330, 588]],
               [[100, 569], [25, 519], [25, 569]],
               [[13, 392], [63, 517], [163, 392]],
               [[243, 371], [293, 496], [243, 496]],
@@ -248,24 +287,43 @@ dots_figure =[[[180, 588], [255, 513], [330, 588]],
               [[822, 321], [822, 396], [897, 346], [897, 321]],
               [[834, 420], [909, 370], [909, 495]],
               [[922, 464], [922, 439], [1022, 439], [1022, 589]]]
-centers_match = [[[199,99], [249,280], [191,164], [140, 205], [241, 206],
-                 [197,273],  [136,297], [277,318]],
+
+
+centers_match = [[[199, 99], [249, 280], [191, 164], [140, 205], [241, 206],
+                 [197, 273],  [136, 297], [277, 318]],
 
                  [[564, 317], [632, 315], [565, 380], [633, 379], [548, 453], 
                   [516, 506], [581, 498], [614, 517], [622, 462], [674, 481]],
                  
                  [[882, 82], [986, 74], [961, 150], [871, 160], [1067, 99],
                   [1065, 190], [887, 223], [899, 281], [974, 244]]]
+
+'''
+набор сокращений 
+в случае минигр создает массив из двух элементов 
+первый отвечает за то, что игра идет 
+второй отвечает за то, прошел ли игрок игру
+'''
+done = False
+mgr = Manager()
+
+done1 = (False, False)
+mgr1 = game1.Manager(50, screen)
+
 mgr2 = game2.Manager(dots_figure, screen)
 done2 = (False, False)
 
-mgr4 = game4.Manager(dots_figure,screen)
+mgr4 = game4.Manager(dots_figure, screen)
 done4 = (False, 0)
 
 mgr3 = experiment.Manager(screen)
 done3 = (False, 0)
 
 while not done:
+    '''
+    в зависимости от положения игрока на карте запускает минигру
+    если игрок прошел миниигру то увеличивает счетчик миниигр на 1
+    '''
     clock.tick(150)
     done = mgr.process(pg.event.get(), screen)[0]
     if 0 == mgr.process(pg.event.get(), screen)[1] and not done1[0]:
@@ -279,7 +337,7 @@ while not done:
             pg.display.update()
             mgr.time.change()
         if done1[1] == True:
-            A+=1
+            A += 1
     if 4 == mgr.process(pg.event.get(), screen)[1] and not done2[0]:
         pg.mixer.music.stop() 
         pg.mixer.music.load('felicia.mp3')
@@ -292,7 +350,7 @@ while not done:
             mgr.time.change()
             pg.display.update()
         if done2[1] == True:
-            A+=1
+            A += 1
     if 1 == mgr.process(pg.event.get(), screen)[1] and not done4[0]:
         pg.mixer.music.stop() 
         pg.mixer.music.load('new_rules.mp3')
@@ -304,7 +362,7 @@ while not done:
             pg.display.update() 
             mgr.time.change()
         if done4[1] == 1:
-            A+=1
+            A += 1
     if 2 == mgr.process(pg.event.get(), screen)[1] and not done3[0]:
         pg.mixer.music.stop() 
         pg.mixer.music.load('psy.mp3')
@@ -315,8 +373,8 @@ while not done:
             done3 = mgr3.process(pg.event.get())
             pg.display.update() 
             mgr.time.change()
-        if done3[1] ==1:
-            A+=1
+        if done3[1] == 1:
+            A += 1
 
     pg.display.update()
 
